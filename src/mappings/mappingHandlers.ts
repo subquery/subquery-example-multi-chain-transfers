449baf1362c8c30184e619f5df7c8a03cce4e9aa
James Bayly <bayly.james@gmail.com>
2022-11-30 18:15:39 +1300

    Prettier

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,12 +1,15 @@
-import {SubstrateEvent} from "@subql/types";
-import {Account, AccountBalance, PublicKey, Transfer} from "../types";
-import {Balance, AccountId} from "@polkadot/types/interfaces";
-import {decodeAddress} from "@polkadot/util-crypto";
-import {u8aToHex} from "@polkadot/util"
+import { SubstrateEvent } from "@subql/types";
+import { Account, AccountBalance, PublicKey, Transfer } from "../types";
+import { Balance, AccountId } from "@polkadot/types/interfaces";
+import { decodeAddress } from "@polkadot/util-crypto";
+import { u8aToHex } from "@polkadot/util";
 
 let NETWORK;
 
-export async function ensureAccount(accountId:string, publicKey:string): Promise<void> {
+export async function ensureAccount(
+  accountId: string,
+  publicKey: string
+): Promise<void> {
   const account = await Account.get(accountId);
   if (!account) {
     const newAccount = new Account(accountId);
@@ -17,10 +20,10 @@ export async function ensureAccount(accountId:string, publicKey:string): Promise
   }
 }
 
-export async function ensurePublicKey(pk:string):Promise<void>{
-  const publicKey = await PublicKey.get(pk)
-  if(!publicKey){
-    await new PublicKey(pk.toString()).save()
+export async function ensurePublicKey(pk: string): Promise<void> {
+  const publicKey = await PublicKey.get(pk);
+  if (!publicKey) {
+    await new PublicKey(pk.toString()).save();
   }
 }
 
@@ -30,41 +33,53 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
   const toAddress = event.event.data[1] as AccountId;
   const amount = event.event.data[2];
 
-  const fromPk = u8aToHex(decodeAddress(fromAddress.toString()))
-  const toPk = u8aToHex(decodeAddress(toAddress.toString()))
-  await Promise.all([ensureAccount(fromAddress.toString(),fromPk),ensureAccount(toAddress.toString(),toPk)])
+  const fromPk = u8aToHex(decodeAddress(fromAddress.toString()));
+  const toPk = u8aToHex(decodeAddress(toAddress.toString()));
+  await Promise.all([
+    ensureAccount(fromAddress.toString(), fromPk),
+    ensureAccount(toAddress.toString(), toPk),
+  ]);
 
-  const transfer = new Transfer(`${NETWORK}-${event.block.block.header.number.toNumber()}-${event.idx}`, );
+  const transfer = new Transfer(
+    `${NETWORK}-${event.block.block.header.number.toNumber()}-${event.idx}`
+  );
   transfer.blockNumber = event.block.block.header.number.toBigInt();
   transfer.fromId = toAddress.toString();
-  transfer.fromPkId = fromPk
+  transfer.fromPkId = fromPk;
   transfer.toId = toAddress.toString();
-  transfer.toPkId = toPk
+  transfer.toPkId = toPk;
   transfer.amount = (amount as Balance).toBigInt();
   transfer.network = NETWORK;
   // await Promise.all([updateBalance(transfer.fromId,fromPk,transfer.blockNumber),updateBalance(transfer.toId, toPk,transfer.blockNumber)])
   await transfer.save();
 }
 
-export async function updateBalance(account:string, publicKey:string,blockHeight:bigint){
-  let { data: { free: previousFree }, nonce: previousNonce } = await api.query.system.account(account);
+export async function updateBalance(
+  account: string,
+  publicKey: string,
+  blockHeight: bigint
+) {
+  let {
+    data: { free: previousFree },
+    nonce: previousNonce,
+  } = await api.query.system.account(account);
   const newBalance = new AccountBalance(`${account}-${blockHeight}`);
   newBalance.balance = previousFree.toBigInt();
   newBalance.accountId = account;
   newBalance.network = NETWORK;
   newBalance.publicKeyId = publicKey;
   newBalance.blockNumber = blockHeight;
-  await newBalance.save()
+  await newBalance.save();
 }
 
-
-export async function handlePolkadotEvent(event: SubstrateEvent):Promise<void>{
-  NETWORK = 'polkadot'
-  await handleEvent(event)
+export async function handlePolkadotEvent(
+  event: SubstrateEvent
+): Promise<void> {
+  NETWORK = "polkadot";
+  await handleEvent(event);
 }
 
-
-export async function handleKusamaEvent(event: SubstrateEvent):Promise<void>{
-  NETWORK = 'kusama'
-  await handleEvent(event)
+export async function handleKusamaEvent(event: SubstrateEvent): Promise<void> {
+  NETWORK = "kusama";
+  await handleEvent(event);
 }
